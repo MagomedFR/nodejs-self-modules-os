dbcd6577d07f4a73eae6c62d468901ba0efd198d
MagomedFR <magomed.khaptukae@gmai.com>
2023-07-04 14:02:39 +0300

    gg

---
+++ index.js
@@ -0,0 +1,7 @@
+import os from 'os';
+import f from './welcome.js'
+// console.log(os.homedir())
+// console.log(os.hostname())
+// console.log(os.version())
+// console.log(os.type())
+console.log(f())
---
+++ welcome.js
@@ -0,0 +1,5 @@
+import os from 'os';
+// const os = require('os');
+export default function f (){
+     return`Привет, юзер. Вижу ты зашел с ${os.type()}`
+}
